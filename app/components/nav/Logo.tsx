2b7f0ea5c4ffcb552c570ee81c7deba7405707c5
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-05-12 08:14:07 +1000

    redirect to home in logo button

--- app/components/nav/Logo.tsx
+++ app/components/nav/Logo.tsx
@@ -1,12 +1,26 @@
 /* eslint-disable react/no-unescaped-entities */
 import { TiShoppingCart } from "react-icons/ti";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 type Props = {};
 
 const Logo = (props: Props) => {
+  const router = useRouter();
+
   return (
-    <div className="flex">
-      <TiShoppingCart size={26} />
-      <div>Jeong's market</div>
+    <div className="flex group cursor-pointer" onClick={() => router.push("/")}>
+      <motion.div>
+        <TiShoppingCart
+          size={26}
+          className="group-hover:animate-[movingCart_0.5s_cubic-bezier(0.68,-0.55,0.3,1.55)]"
+        />
+      </motion.div>
+      <div className="overflow-hidden">
+        <div className="group-hover:animate-[movingLetter_0.8s_cubic-bezier(0.68,-0.55,0.3,1.55)]">
+          Jeong's market
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        movingCart: {
+          "0%": { transform: "translateX(-200%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        movingLetter: {
+          "0%": { transform: "translate(-200%)" },
+          "100%": { transform: "translate(0%)" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
